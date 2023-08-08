81a3c3e3d313ea98aac992712567259abdc32c4e
kimdoyoung0319 <kimdoyoung0319@gmail.com>
2023-08-08 13:54:09 +0000

    Revert "Revising solution for better time complexity."
    This reverts commit ebd7a709f7dbfef43ce585f794e54f119498ac4f.

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -1,44 +1,15 @@
-#include <algorithm>
-using namespace std;
-
 class Solution{
 	public:
-		bool comparator(pair<int, int> p, int num){
-			return p.first == num;
-		}
-
 		vector<int> twoSum(vector<int>& nums, int target){
 			vector<int> result(2);
-			vector<pair<int, int>> index_pair(nums.size());
-			for(auto it = nums.begin(); it != nums.end(); it++){
-				index_pair[distance(nums.begin(), it)]
-					= make_pair(*it, distance(nums.begin(), it);
-			}
-
-			sort(index_pair.begin(), index_pair.end());
-			for(auto it = index_pair.begin(); it != index_pair.end(); it++){
-				int complement = target - (*it).first;
-				if(binary_search(index_pair.begin(),
-						 index_pair.end(),
-						 complement,
-						 comparator)
-				   && complement != (*it).first){
-					result[0] = (*it).second;
-					result_it = equal_range(index_pair.begin(),
-								index_pair.end(),
-								complement,
-								comparator)[0];
-					result[1] = (*result_it).second;
+			for(int i = 0; i < nums.size(); i++){
+				for(int j = i + 1; j < nums.size(); j++){
+					if(nums[i] + nums[j] == target){
+						result[0] = i;
+						result[1] = j;
+					}
 				}
 			}
 			return result;
 		}
 };
-
-int main(void){
-	vector<int> nums{2, 7, 11, 15};
-	int target = 9;
-	Solution sol;
-	cout << sol.twoSum(nums, target) << endl;
-	return 0;
-}
